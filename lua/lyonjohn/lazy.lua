613d300f9c71121896c97012f6a0414c9068b4fc
PoJohnX <liangzhanbo06@gmail.com>
2023-12-05 16:52:50 +0800

    修改错误

--- lua/lyonjohn/lazy.lua
+++ lua/lyonjohn/lazy.lua
@@ -1,28 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  {import = "lyonjohn.plugins"},
-  {import = "lyonjohn.plugins"},
-}, {
-   install = {
-    colorscheme = { "nightfly" },
-  },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+require("lazy").setup({ { import = "lyonjohn.plugins" }, { import = "lyonjohn.plugins.lsp" } }, {
+	install = {
+		colorscheme = { "nightfly" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
