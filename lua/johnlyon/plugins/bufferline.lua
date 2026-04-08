24fb1a8a8966a09cb6e954596d7e28e33a40b4f0
梁展波 <liangzhanbo06@gmail.com>
2026-04-08 18:25:00 +0800

    update colors

--- lua/johnlyon/plugins/bufferline.lua
+++ lua/johnlyon/plugins/bufferline.lua
@@ -3,6 +3,65 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
   opts = {
+    highlights = {
+      fill = {
+        bg = "#161616",
+      },
+      background = {
+        bg = "#1b1b1b",
+        fg = "#6f6f6f",
+      },
+      close_button = {
+        bg = "#1b1b1b",
+        fg = "#6f6f6f",
+      },
+      close_button_visible = {
+        bg = "#1b1b1b",
+        fg = "#6f6f6f",
+      },
+      close_button_selected = {
+        bg = "#2a211d",
+        fg = "#e8ddd4",
+      },
+      buffer_selected = {
+        bg = "#2a211d",
+        fg = "#e8ddd4",
+        bold = false,
+        italic = false,
+      },
+      modified = {
+        bg = "#1b1b1b",
+        fg = "#7a3e48",
+      },
+      modified_visible = {
+        bg = "#1b1b1b",
+        fg = "#7a3e48",
+      },
+      modified_selected = {
+        bg = "#2a211d",
+        fg = "#c45508",
+      },
+      tab_selected = {
+        bg = "#2a211d",
+        fg = "#e8ddd4",
+      },
+      tab_close = {
+        bg = "#1b1b1b",
+        fg = "#6f6f6f",
+      },
+      separator = {
+        bg = "#1b1b1b",
+        fg = "#1b1b1b",
+      },
+      separator_selected = {
+        bg = "#2a211d",
+        fg = "#1b1b1b",
+      },
+      separator_visible = {
+        bg = "#1b1b1b",
+        fg = "#1b1b1b",
+      },
+    },
     options = {
       mode = "buffers",
       close_command = "bdelete! %d",
