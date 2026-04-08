3d85dbebb47220c0241dad01b6e9603678c1e16c
梁展波 <liangzhanbo06@gmail.com>
2026-04-08 22:24:05 +0800

    update keymap

--- lua/johnlyon/core/keymaps.lua
+++ lua/johnlyon/core/keymaps.lua
@@ -8,6 +8,7 @@ local keymap = vim.keymap -- for conciseness
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode with jk" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
@@ -22,12 +23,24 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 -- window management
 keymap.set("n", "<leader>sv", "<c-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower split" })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper split" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { desc = "Move to left split from terminal" })
+keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { desc = "Move to lower split from terminal" })
+keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { desc = "Move to upper split from terminal" })
+keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { desc = "Move to right split from terminal" })
 keymap.set("n", "<leader>svt", function()
 	vim.cmd("vsplit | terminal")
 end, { desc = "Split window vertically and open terminal" })
 keymap.set("n", "<leader>sht", function()
 	vim.cmd("split | terminal")
 end, { desc = "Split window horizontally and open terminal" })
+keymap.set("n", "<leader>h=", "<cmd>resize +2<CR>", { desc = "Increase window height" })
+keymap.set("n", "<leader>h-", "<cmd>resize -2<CR>", { desc = "Decrease window height" })
+keymap.set("n", "<leader>v=", "<cmd>vertical resize +2<CR>", { desc = "Increase window width" })
+keymap.set("n", "<leader>v-", "<cmd>vertical resize -2<CR>", { desc = "Decrease window width" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
